4d8adea2b04a3d8c64fd5a27b0741cd81f0cbcbb
Stephane Hosty <stephanehosty@Stephanes-MacBook-Pro.local>
2024-04-30 10:42:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("*****************************");
+        System.out.println("|        MCLEAN STORE       |");
+        System.out.println("|   04/29/2022    02:45pm   |");
+        System.out.println("|   Gallons:      11.48     |");
+        System.out.println("|   Price/gallon: $4.15     |");
+        System.out.println("|   Fuel total:   $47.642   |");
+        System.out.println("*****************************");
+
+
+        System.out.println("\n");
+
+        System.out.println("*********************************\n" +
+                "|        MCLEAN STORE           |\n" +
+                "|   04/29/2022    \" 02:45pm\"     |\n" +
+                "|   Gallons:      \" 11.48 \"     |\n" +
+                "|   Price/gallon: \" $4.15 \"     |\n" +
+                "|   Fuel total:   \" $47.642\"    |\n" +
+                "*********************************");
+
+    }
+
+
+}
+
+
+
+//Write a program that can display the following output on the console:
+//
+//
+//            *****************************
+//            |        MCLEAN STORE       |
+//            |   04/29/2022    02:45pm   |
+//            |   Gallons:      11.48     |
+//            |   Price/gallon: $4.15     |
+//            |   Fuel total:   $47.642   |
+//            *****************************
